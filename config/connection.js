dab8c2a7707694e589a30332f90650a0deffe7d0
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-11-06 03:23:04 -0500

    update code commit

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,19 @@
 var mysql = require('mysql');
-
-// For jawsDB
 var connection;
-if (process.env.JAWSDB_URL) {
-    // DB is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // DB is local on localhost
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'WkmGry}@[h639hFNTy6Mo4?(',
-        database: 'burgers_db'
-    });
+
+// For Heroku Deployment vs. Local MySQL Database
+/*if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
+else{*/
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'WkmGry}@[h639hFNTy6Mo4?(', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+//}
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
 
+// Export the Connection
 module.exports = connection;
