218f53b13fa7dbb071fa74ea615d94c5cfba76c3
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-11-07 00:12:39 -0500

    add Jaws to config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 var mysql = require('mysql');
-// var connection;
+var connection;
 
 // For Heroku Deployment vs. Local MySQL Database
-/*if(process.env.JAWSDB_URL){
+if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-else{*/
-var connection = mysql.createConnection({
+else{
+connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "WkmGry}@[h639hFNTy6Mo4?(",
   database: "burgers_db"
